3c935314410381eb70b38ae7076847c6bb39f230
beamnaeieiza <98155226+beamnaeieiza@users.noreply.github.com>
2023-06-22 12:58:44 +0700

    product update

--- types/post_product.ts
+++ types/post_product.ts
@@ -1,12 +1,12 @@
 export type PostProductDto = {
-      reserve_id: number,
-      product_name: string;
-      product_picture: string;
-      product_description: string;
-      product_time: string;
-      category_id: number;
-      locate_latitude: string;
-      locate_longtitude: string;
-      status: number;
-      reserved_yet: boolean
+  reserve_id: number;
+  product_name: string;
+  product_picture: string;
+  product_description: string;
+  product_time: string;
+  category_id: number;
+  locate_latitude: string;
+  locate_longtitude: string;
+  status: number;
+  reserved_yet: boolean;
 };
