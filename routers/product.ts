4b2be3169af2d00e0fb72e9739671fb2e5966287
beamnaeieiza <98155226+beamnaeieiza@users.noreply.github.com>
2023-06-22 18:02:55 +0700

    updated product

--- routers/product.ts
+++ routers/product.ts
@@ -43,44 +43,15 @@ productRouter.get("/products/:product_id", async (req, res) => {
       id: productId,
     },
     include: {
-      created_by_user: {
-        select: {
-          firstname: true,
-          lastname: true,
-        },
-      },
+      created_by_user: true,
     },
   });
   if (!product) {
     return res.json({ error: "Product not found." });
   }
 
-  // const category = await prisma.category.findUnique({
-  //   where: {
-  //     id: category_id,
-  //   },
-  // });
-  const {
-    created_by_user: { firstname, lastname },
-  } = product;
-  const {
-    product_name,
-    product_picture,
-    product_description,
-    product_time,
-    category_id,
-    locate_range,
-  } = req.body as PostProductDetailsDto;
-
   return res.send({
-    name: product_name,
-    picture_url: product_picture,
-    details: product_description,
-    available_time: product_time,
-    category: category_id,
-    range: locate_range,
-    giver_name: `${firstname} ${lastname}`,
-    // category: product.category,
+    product,
   });
 });
 
@@ -96,6 +67,7 @@ productRouter.get("/getproducts", async (req, res) => {
       created_products: true,
     },
   });
+  console.log("test");
   return res.send(products);
 });
 
